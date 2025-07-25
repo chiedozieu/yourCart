5616081fb0189a291ef2611353927cdc2853b2cb
chiedozieu <chiedozieugwor@gmail.com>
2025-07-24 20:30:15 -0400

    Implement Navbar and MainBanner components; update App and Home pages; add context for user state management; restructure CSS and .gitignore

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,23 +1,26 @@
-import React, { createContext, useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-    const navigate = useNavigate()
-    const [user, setUser] = useState(null)
-    const [isSeller, setIsSeller] = useState(false)
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
 
-
-    const value = {navigate, user, setUser, isSeller, setIsSeller}
-return (
-
-    <AppContext.Provider value={value}>
-        {children}
-    </AppContext.Provider>
-)
-}
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
 export const useAppContext = () => {
-   return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
