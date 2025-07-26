2659b403b8ee7b3f035900c1d4f0ba72b23fb62f
chiedozieu <chiedozieugwor@gmail.com>
2025-07-25 20:45:03 -0400

    Add product management features and enhance UI components
    - Implement product fetching, adding, updating, and removing in AppContext
    - Introduce BestSellers and Categories components to Home page
    - Add BottomBanner and ProductCard components for improved product display
    - Integrate react-hot-toast for user notifications
    - Update .gitignore and package.json for new dependencies

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,63 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/greencart_assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+  //fetch all products
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  // add product to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+      toast.success("Added to cart");
+    }
+    setCartItems(cartData);
+    
+  };
+
+  // update cart item quantity
+
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart updated");
+  };
+
+  // remove product from cart
+
+  const removeCartItem = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+          delete cartData[itemId];
+          toast.success("Removed from cart");
+      }
+    }
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   const value = {
     navigate,
@@ -17,6 +67,12 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     showUserLogin,
     setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeCartItem,
+    cartItems,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import MainBanner from "../components/MainBanner";
+import Categories from "../components/Categories";
+import BottomBanner from "../components/BottomBanner";
+import BestSellers from "../components/BestSellers";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <MainBanner />
+      <Categories />
+      <BestSellers />
+      <BottomBanner />
+      
     </div>
   );
 };
